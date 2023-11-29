96bbc3288b9dc32f83c2b22f672d5d38928906fa
Jialiang Chen <jialiang.chen@estudiants.urv.cat>
2023-11-29 18:31:43 +0100

    Added content to CustomerService and GameService

--- src/java/service/CustomerService.java
+++ src/java/service/CustomerService.java
@@ -2,6 +2,15 @@ package service;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.ws.rs.Consumes;
+import java.util.List;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 import model.entities.Customer;
 
 /**
@@ -16,6 +25,27 @@ public class CustomerService extends AbstractFacade<Customer>{
         super(Customer.class);
     }
     
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Customer> findAll(){
+        return super.findAll();
+    }
+    
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response find(@PathParam("id") Long id) {
+        return Response.ok().entity(super.find(id)).build();
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Long id, Customer entity) {
+        super.edit(entity);
+    }
+   
     @Override
     protected EntityManager getEntityManager() {
         return em;
--- src/java/service/GameService.java
+++ src/java/service/GameService.java
@@ -2,7 +2,16 @@ package service;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
 import model.entities.Game;
+import model.entities.Game.Console;
+import model.entities.Game.Type;
 
 /**
  *
@@ -15,11 +24,32 @@ public class GameService extends AbstractFacade<Game>{
     public GameService() {
         super(Game.class);
     }
-
+    
+    @GET
+    @Path("/{type}/{console}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public List<Game> findAll(@PathParam("type") Type type, @PathParam("console") Console console){
+    List<Game> games = super.findAll();
+    
+    for(int i = 0; i< games.size(); i++){
+        if(games.get(i).getType().equals(type) || games.get(i).getConsole().equals(console)){
+            games.remove(games.get(i));   
+        }
+    }
+    return games;
+    }
+    
+    @POST
+    @Path("{game}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Override
+    public void create(@PathParam("game") Game entity) {
+        super.create(entity);
+    }
+    
     @Override
     protected EntityManager getEntityManager() {
         return em;
     }
-    
     
 }
