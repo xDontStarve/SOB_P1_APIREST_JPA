04f9bc779601a8f5b902b3324b835403362f444b
Ivan Garcia <ivan.garciap@gmail.com>
2023-11-29 18:04:34 +0100

    algunos rest

--- src/java/service/CustomerService.java
+++ src/java/service/CustomerService.java
@@ -2,6 +2,15 @@ package service;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.ws.rs.Consumes;
+import java.util.List;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 import model.entities.Customer;
 
 /**
@@ -15,9 +24,30 @@ public class CustomerService extends AbstractFacade<Customer>{
     public CustomerService() {
         super(Customer.class);
     }
-
+    
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Customer> findAll(){
+        return super.findAll();
+    }
+    
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response find(@PathParam("id") Long id) {
+        return Response.ok().entity(super.find(id)).build();
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Long id, Customer entity) {
+        super.edit(entity);
+    }
+    
     @Override
     protected EntityManager getEntityManager() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return em;
     }
 }
