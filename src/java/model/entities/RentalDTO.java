e23218cdb838a638e3725aaa38709f4743f86ad1
Jialiang Chen <jialiang.chen@estudiants.urv.cat>
2023-12-02 13:14:49 +0100

    DTO files added

---
+++ src/java/model/entities/RentalDTO.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author chali
+ */
+public class RentalDTO {
+    private long id;
+    private float price;
+    private Date date;
+
+    public RentalDTO(Rental rental){
+        this.id=rental.getId();
+        this.price=rental.getPrice();
+        this.date=rental.getDate();
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+    
+    
+    
+}
